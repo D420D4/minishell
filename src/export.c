d99066edc9d688bb31b58aa4f377334807c03ffb
anonylouis <louis.calvie@gmail.com>
2022-03-14 02:30:51 +0100

    Changements sur export export tout court affiche l env (et remplace les \" par des \\\" c est style) export mdr+=lol ajoute lol au contenu de $mdr, et si $mdr avait pas de contenu, reagit comme export mdr=lol mise a jour de RESTEAFAIRE : plus grand chose !!

--- src/export.c
+++ src/export.c
@@ -6,7 +6,7 @@
 /*   By: lcalvie <lcalvie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 02:22:17 by lcalvie           #+#    #+#             */
-/*   Updated: 2022/03/13 01:27:56 by lcalvie          ###   ########.fr       */
+/*   Updated: 2022/03/14 02:29:06 by lcalvie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,67 @@ static int	is_valid_identifier(char *name, int j)
 	return (0);
 }
 
-int cmd_export(t_cmd *cmd, t_data *data)
+char	*ft_strdup_without_plus(const char *s)
+{
+	int		i;
+	int		plus;
+	char	*ss;
+
+	if (!s)
+		return (NULL);
+	i = ft_strlen(s);
+	ss = (char *)malloc(sizeof(char) * i);
+	if (!ss)
+		return (0);
+	plus = 0;
+	i = 0;
+	while (s[i])
+	{
+		if (!plus && s[i] == '+')
+			plus = 1;
+		else
+			ss[i - plus] = s[i];
+		i++;
+	}
+	ss[i - plus] = s[i];
+	return (ss);
+}
+
+void	print_env_export(t_cmd *cmd, t_data *data)
+{
+	t_list *env;
+	int	i;
+	char	*content;
+	
+	env = data->env;
+	while (env)
+	{
+		content = (char *) env->content;
+		ft_putstr_fd("declare -x ", cmd->fd_out);
+		i = -1;
+		while (content[++i] != '=')
+			ft_putchar_fd(content[i], cmd->fd_out);
+		ft_putstr_fd("=\"", cmd->fd_out);
+		while(content[++i])
+		{
+			if (content[i] == '"')
+				ft_putstr_fd("\\\"", cmd->fd_out);
+			else
+				ft_putchar_fd(content[i], cmd->fd_out);
+		}
+		ft_putstr_fd("\"\n", cmd->fd_out);
+		env = env->next;
+	}
+}
+
+int	cmd_export(t_cmd *cmd, t_data *data)
 {
 	int i;
 	int j;
 	int added;
 	int ret;
 	t_list *lst;
+	char	*new_content;
 
 	i = 0;
 	ret = 0;
@@ -39,26 +93,42 @@ int cmd_export(t_cmd *cmd, t_data *data)
 	{
 		added = 0;
 		j = 0;
-		while (cmd->cmd[i][j] && cmd->cmd[i][j] != '='
+		while (cmd->cmd[i][j] && cmd->cmd[i][j] != '=' && ft_memcmp(cmd->cmd[i] + j,"+=", 2)
 			&& is_valid_identifier(cmd->cmd[i], j))
 			j++;
-		if (cmd->cmd[i][j] == '=' && j != 0)
+		if ((cmd->cmd[i][j] == '='  || !ft_memcmp(cmd->cmd[i] + j,"+=", 2)) && j != 0)
 		{
 			lst = data->env;
 			while (lst && !added)
 			{
 				if (!ft_strncmp(cmd->cmd[i], (char *) lst->content, j))
+					added = 1;
+				else
+					lst = lst->next;
+			}
+			if (added)
+			{
+				if (cmd->cmd[i][j] == '=')
+					new_content = ft_strdup(cmd->cmd[i]);
+				else
+					new_content = ft_strjoin(lst->content, cmd->cmd[i] + j + 2);
+				if (new_content)
 				{
 					free(lst->content);
-					lst->content = strdup(cmd->cmd[i]);
-					added = 1;
+					lst->content = new_content;
 				}
-				lst = lst->next;
 			}
-			if (!added)
-				ft_lstadd_back(&(data->env), ft_lstnew(ft_strdup(cmd->cmd[i])));
+			else
+			{
+				if (cmd->cmd[i][j] == '=')
+					new_content = ft_strdup(cmd->cmd[i]);
+				else
+					new_content = ft_strdup_without_plus(cmd->cmd[i]);
+				if (new_content)
+					ft_lstadd_back(&(data->env), ft_lstnew(new_content));
+			}
 		}
-		else//if (cmd->cmd[i][j] && !is_valid_identifier(cmd->cmd[i], j))
+		else if (cmd->cmd[i][j] || j == 0)
 		{
 			ft_putstr_fd("minishell: export: `", 2);
 			ft_putstr_fd(cmd->cmd[i], 2);
@@ -66,5 +136,7 @@ int cmd_export(t_cmd *cmd, t_data *data)
 			ret = 1;
 		}
 	}
+	if (cmd->cmd[1] == NULL)
+		print_env_export(cmd, data);
 	return (ret);
 }
